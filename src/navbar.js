3a3fd7598c88abf359760b63235814ec7e91777f
Tsering Dhondup <tserockon77@hotmail.com>
2022-07-05 16:57:18 +0530

    Add navbar and animate it using anime.jslibrary

--- src/navbar.js
+++ src/navbar.js
@@ -5,19 +5,18 @@ function createNavbar() {
     const container = document.getElementById('content');
     const content = document.getElementById('home-wrapper');
     const newDiv = document.createElement("div");
-    newDiv.setAttribute('id', 'navbar-bar');
+    newDiv.classList.add('navbar-nav');
     newDiv.innerHTML = `<div class='container'>
-                        <div class="menu-wrapper"><a href="">Reservation</a></div>
-                        <div class="icon-wrapper">
+                        <div class="menu-container"><a href="">Reservation</a></div>
+                        <a class="icon-wrapper">
                         <div class="icon-top"></div>
-                        <div class="icon-bottom"></div></div>
-                        <div class="nav-link-wrapper">
-                        <a href="">FOOD & DRINKS</a>
-                        <a href="">LIBRARY</a>
-                        <a href="">EVENTS</a>
-                        <a href="">MUSIC STUDIO</a></div>
-                        </div>
-                        <h2>WOW<br>&nbsp;CAFE</h2>
+                        <div class="icon-bottom"></div></a>
+                        <div class="menu-link-wrapper">
+                        <div class="list-item first"><a class="text-link first" href="">FOOD & DRINKS</a></div>
+                        <div class="list-item second"><a class="text-link second" href="">LIBRARY</a></div>
+                        <div class="list-item third"><a class="text-link third" href="">EVENTS</a></div>
+                        <div class="list-item forth"><a class="text-link forth" href="">MUSIC STUDIO</a></div></div>
+                        <h2 class="menu-heading">WOW<br>&nbsp;CAFE</h2>
                         </div>`;
     
     container.appendChild(newDiv);
