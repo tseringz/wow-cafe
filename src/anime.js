90f968db01a84701378158d710fb043391b38b19
tseringz <tserockon77@gmail.com>
2022-07-05 16:57:18 +0530

    Add navbar and animate it using anime.jslibrary

--- src/anime.js
+++ src/anime.js
@@ -1,4 +1,4 @@
-function headingAnimation() {
+ function headingAnimation() {
     anime.timeline({loop: false})
     .add({
       targets: '.heading1 .word',
@@ -15,12 +15,102 @@ function headingAnimation() {
       delay: 1000
     });
 };
-export default headingAnimation;
 
-
-function navbarAnimation() {
+ function navbarAnimation() {
+  // navbar scroll down animation
     anime({
         targets: '.navbar-nav ',
-        translateX: 250
+        translateY: '100%',
+        easing: 'easeInOutExpo',
+        duration: 2000,
       });
+      // humberger menu animation
+      anime({
+        targets: '.icon-top',
+        translateY: 4,
+        rotate: 45,
+        easing: 'easeInOutQuad',
+        duration: 1000,
+        delay: 500
+      });
+
+      anime({
+        targets: '.icon-bottom',
+        translateY: -4,
+        rotate: -45,
+        easing: 'easeInOutQuad',
+        duration: 2000,
+        delay: 500
+      });
+
+      // Nav link animation 
+      var animation = anime({
+        targets: '.list-item',
+        translateY: 40,
+        duration: 2000,
+        delay: function(el, i) { return i * 300; },
+        direction: 'alternate',
+        loop: false,
+        easing: 'easeInOutSine',
+      });
+
+      //menu heading animation
+      anime({
+        targets: '.menu-heading',
+        translateX: '-100%',
+        easing: 'easeInOutExpo',
+        duration: 2000,
+      });
+      
 };
+
+function navbarCloseAnimation() {
+  // navbar scroll down animation
+    anime({
+        targets: '.navbar-nav',
+        translateY: '-100%',
+        easing: 'easeInOutExpo',
+        duration: 2000,
+      });
+      // humburger menu animation
+      anime({
+        targets: '.icon-top',
+        translateY: 0,
+        rotate: 0,
+        easing: 'easeInOutQuad',
+        duration: 2000,
+        delay: 500
+      });
+
+      anime({
+        targets: '.icon-bottom',
+        translateY: 0,
+        rotate: 0,
+        easing: 'easeInOutQuad',
+        duration: 2000,
+        delay: 500
+      });
+
+      // Nav link animation 
+      var animation = anime({
+        targets: '.list-item',
+        translateY: 0,
+        duration: 2000,
+        delay: function(el, i) { return i * 300; },
+        direction: 'alternate',
+        loop: false,
+        easing: 'easeInOutSine',
+      });
+
+      // menu-heading animation
+       //menu heading animation
+       anime({
+        targets: '.menu-heading',
+        translateX: '0%',
+        easing: 'easeInOutExpo',
+        duration: 2000,
+      });
+
+}
+
+export { headingAnimation, navbarAnimation, navbarCloseAnimation }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import createHomePage from './home';
 import createFoodPage from './foods';
-import headingAnimation from './anime';
 import createNavbar from './navbar';
+import { headingAnimation, navbarAnimation, navbarCloseAnimation } from './anime';
 import './style/main.scss';
 
 // 
 createHomePage();
 headingAnimation();
+createNavbar();
 
-function tabMenu(){
+function openMenu(){
     const menuItems = document.querySelector('.menu-btn');
     menuItems.addEventListener('click', function(e){
         e.preventDefault();
-        createNavbar();
+        navbarAnimation();
     });
 
 }
-tabMenu();
+openMenu();
+
+function closeMenu() {
+    const menuBar = document.querySelector('.icon-wrapper');
+    menuBar.addEventListener('click', function(e){
+        e.preventDefault();
+        navbarCloseAnimation();
+    });
+
+}
+closeMenu();
+
+
 
